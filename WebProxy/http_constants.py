968363e7bdd139fff652232bcd99d425f6cc6db2
Nikhil Ghosh <nrghosh@users.noreply.github.com>
2018-12-08 16:51:45 -0500

    Update http_constants.py

--- WebProxy/http_constants.py
+++ WebProxy/http_constants.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 #
-# Wesleyan University
-# COMP 332, Spring 2018
+# Credit: Victoria Manfredi
 #
 # A few HTTP constants used to clean up main code
 #
-# Nikhil Ghosh
 
 END_LINE = '\r\n'
 END_HEADER = '\r\n\r\n'
